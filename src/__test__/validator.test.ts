6dfda95885f026b602c0fee57c7e05e4905016b0
HoseungJang <hsjang.dev@gmail.com>
2021-12-15 02:31:11 +0900

    add test cases for validator

---
+++ src/__test__/validator.test.ts
@@ -0,0 +1,115 @@
+import { expect } from "chai";
+
+import { Validator } from "../validator";
+import { Schema } from "../schema";
+
+describe("Validator", () => {
+  describe("Validator.validate", () => {
+    context("when NumberType is passed", () => {
+      const schema = Schema.Number();
+
+      it("should return true if value is evaluated as number", () => {
+        expect(Validator.validate(schema, 1234)).to.be.true;
+      });
+
+      it("should return false if value isn't evaluated as number", () => {
+        expect(Validator.validate(schema, "1234")).to.be.false;
+      });
+    });
+
+    context("when StringType is passed", () => {
+      const schema = Schema.String();
+
+      it("should return true if value is evaluated as string", () => {
+        expect(Validator.validate(schema, "1234")).to.be.true;
+      });
+
+      it("should return false if value isn't evaluated as string", () => {
+        expect(Validator.validate(schema, 1234)).to.be.false;
+      });
+    });
+
+    context("when BooleanType is passed", () => {
+      const schema = Schema.Boolean();
+
+      it("should return true if value is evaluated as boolean", () => {
+        expect(Validator.validate(schema, true)).to.be.true;
+        expect(Validator.validate(schema, false)).to.be.true;
+      });
+
+      it("should return false if value isn't evaluated as boolean", () => {
+        expect(Validator.validate(schema, "1234")).to.be.false;
+      });
+    });
+
+    context("when NullType is passed", () => {
+      const schema = Schema.Null();
+
+      it("should return true if value is evaluated as null", () => {
+        expect(Validator.validate(schema, null)).to.be.true;
+      });
+
+      it("should return false if value isn't evaluated as null", () => {
+        expect(Validator.validate(schema, "1234")).to.be.false;
+      });
+    });
+
+    context("when EnumType is passed", () => {
+      const schema = Schema.Enum(["a", "b", "c"]);
+
+      it("should return true if value is evaluated as enum", () => {
+        expect(Validator.validate(schema, "a")).to.be.true;
+        expect(Validator.validate(schema, "b")).to.be.true;
+        expect(Validator.validate(schema, "c")).to.be.true;
+      });
+
+      it("should return false if value isn't evaluated as enum", () => {
+        expect(Validator.validate(schema, "1234")).to.be.false;
+      });
+    });
+
+    context("when ArrayType is passed", () => {
+      const schema = Schema.Array(Schema.Number());
+
+      it("should return true if value is evaluated as array", () => {
+        expect(Validator.validate(schema, [1, 2, 3, 4])).to.be.true;
+      });
+
+      it("should return false if value isn't evaluated as array", () => {
+        expect(Validator.validate(schema, ["a", "b", "c", "d"])).to.be.false;
+        expect(Validator.validate(schema, "1234")).to.be.false;
+      });
+    });
+
+    context("when UnionType is passed", () => {
+      const schema = Schema.Union([Schema.Number(), Schema.String()]);
+
+      it("should return true if value is evaluated as union", () => {
+        expect(Validator.validate(schema, 1234)).to.be.true;
+        expect(Validator.validate(schema, "1234")).to.be.true;
+      });
+
+      it("should return false if value isn't evaluated as union", () => {
+        expect(Validator.validate(schema, [])).to.be.false;
+        expect(Validator.validate(schema, {})).to.be.false;
+      });
+    });
+
+    context("when ObjectType is passed", () => {
+      const schema = Schema.Object({ number: Schema.Number(), optional: Schema.Optional(Schema.String()) });
+
+      it("should return true if value is evaluated as union", () => {
+        expect(Validator.validate(schema, { number: 1234 })).to.be.true;
+        expect(Validator.validate(schema, { number: 1234, optional: "1234" })).to.be.true;
+      });
+
+      it("should return false if value isn't evaluated as union", () => {
+        expect(Validator.validate(schema, {})).to.be.false;
+        expect(Validator.validate(schema, [])).to.be.false;
+        expect(Validator.validate(schema, null)).to.be.false;
+        expect(Validator.validate(schema, { number: "1234" })).to.be.false;
+        expect(Validator.validate(schema, { number: 1234, optional: 1234 })).to.be.false;
+      });
+    });
+  });
+});
