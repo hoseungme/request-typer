4579e75ef81e84d6f41d010674bbe8a6ff585a0e
HoseungJang <hsjang.dev@gmail.com>
2021-12-18 08:53:20 +0900

    fix result format of validate method

--- src/validator.ts
+++ src/validator.ts
@@ -1,44 +1,83 @@
 import { AllType } from "./schema";
 
+export class ValidationError {
+  constructor(public readonly description: string) {}
+}
+
+interface SuccessResult {
+  success: true;
+}
+
+interface FailedResult {
+  success: false;
+  error: ValidationError;
+}
+
+export type ValidationResult = SuccessResult | FailedResult;
+
 export class Validator {
-  public static validate(schema: AllType, value: any): boolean {
+  public static validate(schema: AllType, value: any): ValidationResult {
     if (value === undefined) {
-      return !!schema.options.optional;
+      return !!schema.options.optional ? this.makeResult(true) : this.makeResult(false, "should be provided");
     }
 
     switch (schema.type) {
       case "number": {
-        return typeof value === "number";
+        return typeof value === "number"
+          ? this.makeResult(true)
+          : this.makeResult(false, `should be ${schema.definition}`);
       }
       case "string": {
-        return typeof value === "string";
+        return typeof value === "string"
+          ? this.makeResult(true)
+          : this.makeResult(false, `should be ${schema.definition}`);
       }
       case "boolean": {
-        return typeof value === "boolean";
+        return typeof value === "boolean"
+          ? this.makeResult(true)
+          : this.makeResult(false, `should be ${schema.definition}`);
       }
       case "enum": {
-        return schema.values.some((v) => v === value);
+        return schema.values.some((v) => v === value)
+          ? this.makeResult(true)
+          : this.makeResult(false, `should be one of ${schema.definition}`);
       }
       case "array": {
         const isArray = value instanceof Array;
         if (!isArray) {
-          return false;
+          return this.makeResult(false, "should be array");
         }
-        return value.every((item) => this.validate(schema.itemType, item));
+        return value.every((item) => this.validate(schema.itemType, item).success)
+          ? this.makeResult(true)
+          : this.makeResult(false, `should be ${schema.definition}`);
       }
       case "union": {
-        return schema.itemTypes.some((item) => this.validate(item, value));
+        return schema.itemTypes.some((item) => this.validate(item, value).success)
+          ? this.makeResult(true)
+          : this.makeResult(false, `should be ${schema.definition}`);
       }
       case "object": {
         const isObject = typeof value === "object" && value !== null && !(value instanceof Array);
         if (!isObject) {
-          return false;
+          return this.makeResult(false, "should be object");
         }
-        const keys = Object.keys(schema.properties);
-        return keys.every((key) => {
-          return this.validate(schema.properties[key], value[key]);
-        });
+
+        const failedResults = Object.keys(schema.properties)
+          .map((key) => [key, this.validate(schema.properties[key], value[key])] as const)
+          .filter((result) => !result[1].success) as (readonly [string, FailedResult])[];
+        return failedResults.length === 0
+          ? this.makeResult(true)
+          : this.makeResult(
+              false,
+              failedResults.map((pair) => `property [${pair[0]}]: ${pair[1].error.description}`).join(", ")
+            );
       }
     }
+  }
+
+  private static makeResult(success: true): ValidationResult;
+  private static makeResult(success: false, description: string): ValidationResult;
+  private static makeResult(success: boolean, description?: string): ValidationResult {
+    return success ? { success: true } : { success: false, error: new ValidationError(description!) };
   }
 }
